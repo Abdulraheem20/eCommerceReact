85d2ef2bcd232c9cc45fc702a22989ff901b685d
Abdulraheem <raimitaiwo45@gmail.com>
2022-11-26 16:05:46 +0100

    newCommitment

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import { lazy, Suspense } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Footer from "./components/Footer";
-import Navbarr from "./components/Navbarr";
-import "./App.css";
 import ShoppingCartTable from "./components/ShoppingCartTable";
 
 const Home = lazy(() => import("../src/components/pages/Home"));
-const Product = lazy(() => import("./components/pages/Products"));
 const Login = lazy(() => import("./components/ShowLogin"));
 const Shop = lazy(() => import("./components/pages/Shop"));
 const Blog = lazy(() => import("./components/pages/Blog"));
 const Contact = lazy(() => import("./components//pages/Contact"));
 const Detailed = lazy(() => import("./components/DetailedDesc"));
-const CartTable = lazy(() => import("./components/ShoppingCartTable"));
 
 function App() {
   return (
